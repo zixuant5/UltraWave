246e5e3328ff4cffec41a96c769596a9f8d2be58
Zixuan Tian <zixuant5@illinois.edu>
2025-06-29 21:54:56 -0700

    remove build artifacts

--- build/lib/ultrawave/__init__.py
+++
@@ -1,3 +0,0 @@
-from .lib.source import TimeAxis, ToneBurstSource, Receiver
-from .lib.operator import Acoustic2DOperator
-from .utils import plot_velocity
--- build/lib/ultrawave/lib/__init__.py
+++
@@ -1,4 +0,0 @@
-from .source import *
-from .operator import *
-from .model_2d import *
-from .model_3d import *
